22bebf2e3ec62567d13fb8d17430352719849648
rakibtg <rakibtg@gmail.com>
2018-10-21 18:47:56 +0600

    initial commits

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+const { ipcRenderer } = window.require("electron")
 
 class App extends Component {
+
+  state = {
+    items: [],
+  }
+
+  componentDidMount() {
+    ipcRenderer.send('asynchronous-message', 'initial-data')
+    ipcRenderer.on('electron-to-react', this.handleElectronRequests.bind(this))
+  }
+
+  handleElectronRequests(event, arg) {
+    const data = JSON.parse(arg)
+    if(data.eventType === 'initial-data') {
+      const itemLists = data.items.split("\n")
+      // console.log(itemLists)
+      const re = /(\s+\s+)/
+      itemLists.map(i => console.log(i.split(re)))
+      // console.log(itemLists[0])
+      // console.log(itemLists[0].split(re))
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <div>
+            Docker Elementary
+          </div>
         </header>
       </div>
     );
