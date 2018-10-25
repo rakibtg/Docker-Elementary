d67f35aedb62b599dd12db98b6dfe7ece67ad936
Kazi Mehedi Hasan <rakibtg@gmail.com>
2018-10-25 11:23:24 +0600

    registered root index with redux

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-ReactDOM.render(<App />, document.getElementById('root'));
-serviceWorker.unregister();
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+// https://github.com/rakibtg/React-Native-Redux-Example
+import {Provider} from 'react-redux'
+import {createStore, applyMiddleware} from 'redux'
+import reducers from './reducers'
+
+const createStoreWithMiddleware = applyMiddleware()(createStore)
+
+ReactDOM.render(<Provider store={createStoreWithMiddleware(reducers)}>
+  <App />
+</Provider>, document.getElementById('root'));
