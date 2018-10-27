59bbcc11dfd4bde99febcb003503df16b9b3c505
rakibtg <rakibtg@gmail.com>
2018-10-27 12:15:54 +0600

    connect the app with store

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-// https://github.com/rakibtg/React-Native-Redux-Example
+import initStore from './store'
 import {Provider} from 'react-redux'
-import {createStore, applyMiddleware} from 'redux'
-import reducers from './reducers'
 
-const createStoreWithMiddleware = applyMiddleware()(createStore)
+const store = initStore()
 
-ReactDOM.render(<Provider store={createStoreWithMiddleware(reducers)}>
-  <App />
-</Provider>, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App/>
+  </Provider>, 
+  document.getElementById('root')
+)
