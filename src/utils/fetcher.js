34c631fb9f02639e89bf2919d2302d7163a742c7
rakibtg <rakibtg@gmail.com>
2018-10-29 23:18:59 +0600

    communicator doc

--- src/utils/fetcher.js
+++ src/utils/fetcher.js
@@ -2,6 +2,8 @@ import {store} from '../index'
 const {ipcRenderer} = window.require('electron')
 
 export default (dataAction, loadingAction = null, errorAction = null, payloads = {}) => {
+  // Collect all data to be set in a store.
+  // Dispatch collected data in the store VIA action.
   store.dispatch(dataAction(payloads))
   if(loadingAction) store.dispatch(loadingAction())
   if(errorAction) store.dispatch(errorAction)
