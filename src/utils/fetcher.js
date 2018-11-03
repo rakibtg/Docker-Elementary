9edc5bb8dfaa0b544607c9252224eddefbd2f5f3
rakibtg <rakibtg@gmail.com>
2018-11-03 13:14:50 +0600

    re-architecting

--- src/utils/fetcher.js
+++ src/utils/fetcher.js
@@ -1,10 +1,26 @@
 import {store} from '../index'
 const {ipcRenderer} = window.require('electron')
 
-export default (dataAction, loadingAction = null, errorAction = null, payloads = {}) => {
+// const recipes = {
+//   'active-containers': () => {
+//     ipcRenderer.send('asynchronous-message', JSON.stringify({
+//       type: 'fetch-containers',
+//       options: {
+//         filter,
+//       }
+//     }))
+//     ipcRenderer.on('electron-to-react', this.handleElectronRequests.bind(this))
+//   }
+// }
+
+export default async (source, action) => {
+  setTimeout(async () => {
+    const data = await source()
+  }, 5000);
+  console.log('Data from fetcher', data)
   // Collect all data to be set in a store.
   // Dispatch collected data in the store VIA action.
-  store.dispatch(dataAction(payloads))
-  if(loadingAction) store.dispatch(loadingAction())
-  if(errorAction) store.dispatch(errorAction)
+  // store.dispatch(dataAction(payloads))
+  // if(loadingAction) store.dispatch(loadingAction())
+  // if(errorAction) store.dispatch(errorAction)
 }
