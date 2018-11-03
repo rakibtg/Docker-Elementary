a5ec886ea50e67b95b43820558cfebfb170dff6e
rakibtg <rakibtg@gmail.com>
2018-11-03 13:14:20 +0600

    testing reactToElectron

--- src/screens/ContainerScreen.js
+++ src/screens/ContainerScreen.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
-import fetcher from '../utils/fetcher'
+// import fetcher from '../utils/fetcher'
 import { getContainers } from '../actions/container'
+
+import reactToElectron from '../utils/reactToElectron'
 
 class ContainerScreen extends Component {
 
-  componentDidMount() {
-    console.log('Inside Containers')
-    fetcher(getContainers)
+  async componentDidMount() {
+    // fetcher(source, action)
+    const containers = await reactToElectron('fetch-containers', { filter: 'active', })
+    console.log('Super containers: ', containers)
   }
 
   render() {
