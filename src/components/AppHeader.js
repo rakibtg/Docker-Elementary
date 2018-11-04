ba50205ea938b3eb4d1ffd9e7760b6cd5ad03390
rakibtg <rakibtg@gmail.com>
2018-11-04 15:52:31 +0600

    clean up the global header

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,62 +1,24 @@
 import React, { Component } from 'react'
-import { Pane, Spinner, SegmentedControl, Text, Icon, Badge } from 'evergreen-ui'
-import {bindActionCreators} from 'redux'
-import {connect} from 'react-redux'
+import { Pane } from 'evergreen-ui'
 
-import {setScreen} from '../actions/screen'
+import FilterContainers from './FilterContainers'
+import ScreenNavigator from './ScreenNavigator'
 
 class AppHeader extends Component {
-  state = {
-    loadingFilter: '',
-    currentFilterForContainer: 'all'
-  }
   render() {
-    const {loadingFilter, currentFilterForContainer} = this.state
-    const {screen, setScreen} = this.props
     return <div className='app-header'>
       <Pane display="flex" padding={16} background="tint1">
         <Pane display="flex">
         </Pane>
         <Pane flex={1} display="flex" alignItems="center" justifyContent="center">
-          <SegmentedControl
-            width={750}
-            height={50}
-            options={[
-              { label: <Text display='flex' alignItems='center'><Icon size={14} color="muted" icon="layers" marginRight={5}/> Container <Badge color="neutral" marginLeft={5}>29</Badge></Text>, value: 'container' },
-              { label: <Text display='flex' alignItems='center'><Icon size={14} color="muted" icon="projects" marginRight={5}/> Image <Badge color="neutral" marginLeft={5}>8</Badge></Text>, value: 'image' },
-              { label: <Text display='flex' alignItems='center'><Icon size={14} color="muted" icon="database" marginRight={5}/> Volume <Badge color="neutral" marginLeft={5}>9</Badge></Text>, value: 'volume' },
-              { label: <Text display='flex' alignItems='center'><Icon size={14} color="muted" icon="cell-tower" marginRight={5}/> Network <Badge color="neutral" marginLeft={5}>4</Badge></Text>, value: 'network' },
-              { label: <Text display='flex' alignItems='center'><Icon size={14} color="muted" icon="shield" marginRight={5}/> Clean-up</Text>, value: 'cleanup' },
-            ]}
-            value={screen}
-            onChange={value => setScreen(value)}
-          />
+          <ScreenNavigator/>
         </Pane>
       </Pane>
       <Pane display='flex' alignItems='center' justifyContent='center' padding={5} background='tint2'>
-        <SegmentedControl
-          width={400}
-          height={24}
-          options={[
-            { label: loadingFilter === 'all' ? <Spinner size={16} /> : 'All', value: 'all' },
-            { label: loadingFilter === 'active' ? <Spinner size={16} /> : 'Active', value: 'active' },
-            { label: loadingFilter === 'stopped' ? <Spinner size={16} /> : 'Stopped', value: 'stopped' }
-          ]}
-          value={currentFilterForContainer}
-          onChange={value => {
-            this.setState({ currentFilterForContainer: value, loadingFilter: value })
-            this.containerFetcher(value)
-          }}
-        />
+        <FilterContainers/>
       </Pane>
     </div>
   }
 }
 
-const mapStateToProps = state => ({
-  screen: state.screen
-})
-
-const mapDispatchToProps = dispatch => bindActionCreators( {setScreen}, dispatch )
-
-export default connect(mapStateToProps, mapDispatchToProps)(AppHeader)
+export default AppHeader
