1e18ada5569c827f88f34fef17f1ea32b5878dbe
rakibtg <rakibtg@gmail.com>
2018-11-04 15:54:53 +0600

    communication bridge for react to electron

--- src/utils/reactToElectron.js
+++ src/utils/reactToElectron.js
@@ -3,7 +3,11 @@ const {ipcRenderer} = window.require('electron')
 export default (type, options) => new Promise((resolve, reject) => {
   ipcRenderer.send('asynchronous-message', JSON.stringify({ type, options }))
   ipcRenderer.on('electron-to-react', (event, arg) => {
-    const data = JSON.parse(arg)
-    resolve(data)
+    try {
+      const data = JSON.parse(arg)
+      resolve(data)
+    } catch (error) {
+      reject('Failed')
+    }
   })
 })
