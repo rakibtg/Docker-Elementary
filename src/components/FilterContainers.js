7ca18878db4fddd6bd1d3155033315cfb636b484
rakibtg <rakibtg@gmail.com>
2018-11-04 15:52:44 +0600

    segment to filter containers

---
+++ src/components/FilterContainers.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import { Spinner, SegmentedControl} from 'evergreen-ui'
+import {connect} from 'react-redux'
+import fetcher from '../utils/fetcher'
+
+class FilterContainers extends Component {
+  state = {
+    currentFilterForContainer: 'active'
+  }
+  render() {
+    const {currentFilterForContainer} = this.state
+    const {loadingContainerFilter, screen} = this.props
+    if(screen != 'container') return null
+    return <SegmentedControl
+      width={400}
+      height={24}
+      options={[
+        { label: loadingContainerFilter === 'all' ? <Spinner size={16} /> : 'All', value: 'all' },
+        { label: loadingContainerFilter === 'active' ? <Spinner size={16} /> : 'Active', value: 'active' },
+        { label: loadingContainerFilter === 'stopped' ? <Spinner size={16} /> : 'Stopped', value: 'stopped' }
+      ]}
+      value={currentFilterForContainer}
+      onChange={value => {
+        this.setState({ currentFilterForContainer: value })
+        fetcher('getContainers', {filter: value})
+      }}
+    />
+  }
+}
+
+const mapStateToProps = state => ({
+  loadingContainerFilter: state.container.loading,
+  screen: state.screen
+})
+
+export default connect(mapStateToProps, null)(FilterContainers)
