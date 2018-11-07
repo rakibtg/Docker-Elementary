d33f20ccfe4f5b0db231e4b2bb21059fb3308db8
rakibtg <rakibtg@gmail.com>
2018-11-08 00:51:32 +0600

    display stat of active containers live

---
+++ src/components/ContainerLiveStats/ContainerLiveStats.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import { Pill } from 'evergreen-ui'
+import { connect } from 'react-redux'
+
+class ContainerLiveStats extends Component {
+
+  getMemoryUsage(memory) {
+    [memory] = memory.split('/')
+    return memory.trim()
+  }
+
+  render() {
+    const { stats, container } = this.props
+    const stat = stats.filter(n => n.id === container)
+    const validStat = stat[0] ? stat[0] : false
+
+    if(!validStat) return null
+    // console.log(validStat)
+    return <>
+      <Pill 
+        paddingLeft={10} 
+        paddingRight={10} 
+        color="blue" 
+        marginRight={8}
+        textTransform='lowercase'>
+        CPU {validStat.cpu_percentage}
+      </Pill>
+      <Pill 
+        paddingLeft={10} 
+        paddingRight={10} 
+        color="purple" 
+        marginRight={8}
+        textTransform='lowercase'
+        title={validStat.memory_usage}>
+        Ram {this.getMemoryUsage(validStat.memory_usage)}
+      </Pill>
+    </>
+  }
+}
+
+const mapStateToProps = state => ({
+  stats: state.container.stats
+})
+export default connect(mapStateToProps, null)(ContainerLiveStats)
