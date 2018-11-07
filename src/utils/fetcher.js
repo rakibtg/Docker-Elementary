2213516fc104ed1129a90c2c394b75b4f3488316
Kazi Mehedi Hasan <rakibtg@gmail.com>
2018-11-07 10:34:14 +0600

    added getContainerStats recipe

--- src/utils/fetcher.js
+++ src/utils/fetcher.js
@@ -1,7 +1,11 @@
 import {store} from '../index'
 import reactToElectron from './reactToElectron'
 
-import {setContainers, setLoadingContainer} from '../actions/container'
+import {
+  setContainers, 
+  setContainerStats,
+  setLoadingContainer,
+} from '../actions/container'
 
 const recipes = {
   getContainers: async options => {
@@ -15,6 +19,11 @@ const recipes = {
     } catch (e) {
       // Dispatch error.
     }
+  },
+  getContainerStats: async () => {
+    const stats = await reactToElectron('fetch-container-stats')
+    store.dispatch(setContainerStats(stats))
+    // console.log('Stats:', stats)
   }
 }
 
