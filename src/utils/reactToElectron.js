31e0af87f53a66a6361ad0ac407c0144015af6d2
rakibtg <rakibtg@gmail.com>
2018-11-08 00:52:08 +0600

    Event listener memory leaks fixed

--- src/utils/reactToElectron.js
+++ src/utils/reactToElectron.js
@@ -1,12 +1,11 @@
 const {ipcRenderer} = window.require('electron')
 
 export default (type, options) => new Promise((resolve, reject) => {
-  console.log('Types:', type+'-message')
+  // console.log('Types:', type+'-message')
   ipcRenderer.send(type+'-message', JSON.stringify({ type, options }))
-  ipcRenderer.on(type, (event, arg) => {
+  ipcRenderer.once(type, (event, arg) => {
     try {
       const data = JSON.parse(arg)
-      console.log('RAW Data:', data)
       resolve(data)
     } catch (error) {
       reject('Failed')
