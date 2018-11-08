a6849b0db2bcbf5dbf08e420aa16cd934a718edf
rakibtg <rakibtg@gmail.com>
2018-11-09 00:07:15 +0600

    removed extra data from initial containers list

--- electron_src/controllers/containers/fetch-containers.js
+++ electron_src/controllers/containers/fetch-containers.js
@@ -1,23 +1,29 @@
 const cmdLikeAPro = require('../../utilities/cmd-pro')
 
+const lightContainerDetail = (id, inspectedData) => ({
+  Id: inspectedData.Id,
+  shortId: id,
+  Created: inspectedData.Created,
+  State: inspectedData.State,
+  Name: inspectedData.Name
+})
+
 module.exports = fetchContainers = (status) => new Promise( async (resolve, reject) => {
   const rawContainersFromCmd = await cmdLikeAPro('docker ps -q -a')
-  let containers = rawContainersFromCmd
+  const containers = rawContainersFromCmd
     .split("\n")
     .map(container => container.trim())
     .filter(container => container !== '')
   let results = {}
   await Promise.all(containers.map(async container => {
     const weAreTheFortunateOne = await cmdLikeAPro('docker container inspect '+container)
-    let tintContainer = JSON.parse(weAreTheFortunateOne)[0]
-    tintContainer['shortId'] = container
-    // Filter containers.
+    const tintContainer = JSON.parse(weAreTheFortunateOne)[0]
     if(status === 'active') {
-      if(tintContainer.State.Running === true) results[container] = tintContainer
+      if(tintContainer.State.Running === true) results[container] = lightContainerDetail(container, tintContainer)
     } else if(status === 'all') {
-      results[container] = tintContainer
+      results[container] = lightContainerDetail(container, tintContainer)
     } else if(status === 'stopped') {
-      if(tintContainer.State.Running !== true) results[container] = tintContainer
+      if(tintContainer.State.Running !== true) results[container] = lightContainerDetail(container, tintContainer)
     }
   }))
   resolve(results)
