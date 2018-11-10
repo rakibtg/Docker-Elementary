23176c635b588c7cbf6d8bdf8909d666b45ed7df
rakibtg <rakibtg@gmail.com>
2018-11-10 11:18:48 +0600

    added fetchContainerByID

--- electron_src/controllers/containers/fetch-containers.js
+++ electron_src/controllers/containers/fetch-containers.js
@@ -8,23 +8,30 @@ const lightContainerDetail = (id, inspectedData) => ({
   Name: inspectedData.Name
 })
 
-module.exports = fetchContainers = (status) => new Promise( async (resolve, reject) => {
-  const rawContainersFromCmd = await cmdLikeAPro('docker ps -q -a')
-  const containers = rawContainersFromCmd
-    .split("\n")
-    .map(container => container.trim())
-    .filter(container => container !== '')
-  let results = {}
-  await Promise.all(containers.map(async container => {
-    const weAreTheFortunateOne = await cmdLikeAPro('docker container inspect '+container)
-    const tintContainer = JSON.parse(weAreTheFortunateOne)[0]
-    if(status === 'active') {
-      if(tintContainer.State.Running === true) results[container] = lightContainerDetail(container, tintContainer)
-    } else if(status === 'all') {
-      results[container] = lightContainerDetail(container, tintContainer)
-    } else if(status === 'stopped') {
-      if(tintContainer.State.Running !== true) results[container] = lightContainerDetail(container, tintContainer)
-    }
-  }))
-  resolve(results)
-})
+module.exports = {
+  fetchContainers: (status) => new Promise( async (resolve, reject) => {
+    const rawContainersFromCmd = await cmdLikeAPro('docker ps -q -a')
+    const containers = rawContainersFromCmd
+      .split("\n")
+      .map(container => container.trim())
+      .filter(container => container !== '')
+    let results = {}
+    await Promise.all(containers.map(async container => {
+      const weAreTheFortunateOne = await cmdLikeAPro('docker container inspect '+container)
+      const tintContainer = JSON.parse(weAreTheFortunateOne)[0]
+      if(status === 'active') {
+        if(tintContainer.State.Running === true) results[container] = lightContainerDetail(container, tintContainer)
+      } else if(status === 'all') {
+        results[container] = lightContainerDetail(container, tintContainer)
+      } else if(status === 'stopped') {
+        if(tintContainer.State.Running !== true) results[container] = lightContainerDetail(container, tintContainer)
+      }
+    }))
+    resolve(results)
+  }),
+  fetchContainerByID: containerID => new Promise(async (resolve, reject) => {
+    const containerInspect = await cmdLikeAPro('docker container inspect ' + containerID)
+    const result = lightContainerDetail(containerID, JSON.parse(containerInspect)[0])
+    resolve(result)
+  })
+}
