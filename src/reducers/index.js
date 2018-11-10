053ec6b55705750fa478f4ec63db2d906f35469c
rakibtg <rakibtg@gmail.com>
2018-11-10 13:49:33 +0600

    registered error reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import {combineReducers} from 'redux'
 import screen from './screen'
 import container from './container'
+import error from './error'
 export default combineReducers({
   screen,
-  container
+  container,
+  error
 })
