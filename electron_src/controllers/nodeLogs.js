eb17ce3ad9442466e9e761899ddc8d886b4eedc4
rakibtg <rakibtg@gmail.com>
2019-03-30 17:06:34 +0600

    Removed older version of container logs

--- electron_src/controllers/nodeLogs.js
+++
@@ -1,7 +0,0 @@
-const cmdLikeAPro = require('../utilities/cmd-pro')
-
-module.exports = getContainerLogs = containerID => new Promise( async (resolve, reject) => {
-  const cmd = `docker container logs ${containerID}`
-  const data = await cmdLikeAPro(cmd)
-  resolve(data)
-})
