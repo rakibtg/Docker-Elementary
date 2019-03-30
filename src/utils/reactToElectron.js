9e491e067e8b014dde701d42171a897f04ac691c
rakibtg <rakibtg@gmail.com>
2019-03-30 17:12:30 +0600

    Errors log

--- src/utils/reactToElectron.js
+++ src/utils/reactToElectron.js
@@ -1,4 +1,4 @@
-const {ipcRenderer} = window.require('electron')
+const { ipcRenderer } = window.require('electron')
 
 export default (type, options) => new Promise((resolve, reject) => {
   ipcRenderer.send(type+'-message', JSON.stringify({ type, options }))
@@ -7,6 +7,7 @@ export default (type, options) => new Promise((resolve, reject) => {
       const data = JSON.parse(arg)
       resolve(data)
     } catch (error) {
+      console.log('reactToElectron.js Error: ', error)
       reject('Failed')
     }
   })
