ecf93c6cfe988990e1dce1c82faa31eba808fbbe
rakibtg <rakibtg@gmail.com>
2019-03-30 17:08:20 +0600

    Refactored the navigator controls

--- src/components/ScreenNavigator.js
+++ src/components/ScreenNavigator.js
@@ -6,17 +6,27 @@ import {setScreen} from '../actions/screen'
 
 class ScreenNavigator extends Component {
 
-  render() {
+  navButton (name, amount, icon) {
+    return <Text display='flex' alignItems='center'>
+      <Icon size={14} color="muted" icon={ icon } marginRight={5}/> 
+        { name } 
+        <Badge color="neutral" marginLeft={5}>
+          { amount }
+        </Badge>
+    </Text>
+  }
+
+  render () {
     const {screen, setScreen} = this.props
     return <SegmentedControl
       width={750}
       height={50}
       options={[
-        { label: <Text display='flex' alignItems='center'><Icon size={14} color="muted" icon="layers" marginRight={5}/> Container <Badge color="neutral" marginLeft={5}>29</Badge></Text>, value: 'container' },
-        { label: <Text display='flex' alignItems='center'><Icon size={14} color="muted" icon="projects" marginRight={5}/> Image <Badge color="neutral" marginLeft={5}>8</Badge></Text>, value: 'image' },
-        { label: <Text display='flex' alignItems='center'><Icon size={14} color="muted" icon="database" marginRight={5}/> Volume <Badge color="neutral" marginLeft={5}>9</Badge></Text>, value: 'volume' },
-        { label: <Text display='flex' alignItems='center'><Icon size={14} color="muted" icon="cell-tower" marginRight={5}/> Network <Badge color="neutral" marginLeft={5}>4</Badge></Text>, value: 'network' },
-        { label: <Text display='flex' alignItems='center'><Icon size={14} color="muted" icon="shield" marginRight={5}/> Clean-up</Text>, value: 'cleanup' },
+        { label: this.navButton( 'Container', 29, 'layers' ), value: 'container' },
+        { label: this.navButton( 'Image', 8, 'projects' ), value: 'image' },
+        { label: this.navButton( 'Volume', 8, 'database' ), value: 'volume' },
+        { label: this.navButton( 'Network', 8, 'cell-tower' ), value: 'network' },
+        { label: this.navButton( 'Clean-up', 8, 'shield' ), value: 'cleanup' }
       ]}
       value={screen}
       onChange={value => setScreen(value)}
